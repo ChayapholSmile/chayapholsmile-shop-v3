6bfaf1266e6c26a531ae1bd6dcf2d180f488b898
ChayapholSmile <151313831+ChayapholSmile@users.noreply.github.com>
2025-08-08 19:51:55 +0700

    Update fees.ts

--- lib/fees.ts
+++ lib/fees.ts
@@ -3,11 +3,11 @@ import type { PaymentMethod } from "./types"
 // Define fee policy per method
 export function calculateFee(amount: number, method: PaymentMethod) {
   // Example fees: you can tune these numbers
-  const base = 2 // THB
+  const base = 100 // THB
   const rateByMethod: Record<PaymentMethod, number> = {
-    "truemoney-angpao": 0.05,
-    "truemoney": 0.035,
-    "promptpay": 0.01,
+    "truemoney-angpao": 0,
+    "truemoney": 0,
+    "promptpay": 0,
   }
   const rate = rateByMethod[method] ?? 0
   const fee = Math.ceil(base + amount * rate)
